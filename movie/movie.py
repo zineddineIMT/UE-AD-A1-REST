31fa0c6375e953e9deef2212ee1640115c60d249
zineddineIMT <146106879+zineddineIMT@users.noreply.github.com>
2023-11-14 14:39:35 +0100

    first-commit
    init of all files

---
+++ movie/movie.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request, jsonify, make_response
+import json
+import sys
+from werkzeug.exceptions import NotFound
+
+app = Flask(__name__)
+
+PORT = 3200
+HOST = '0.0.0.0'
+
+with open('{}/databases/movies.json'.format("."), "r") as jsf:
+   movies = json.load(jsf)["movies"]
+
+# root message
+@app.route("/", methods=['GET'])
+def home():
+    return make_response("<h1 style='color:blue'>Welcome to the Movie service!</h1>",200)
+
+if __name__ == "__main__":
+    #p = sys.argv[1]
+    print("Server running in port %s"%(PORT))
+    app.run(host=HOST, port=PORT)
