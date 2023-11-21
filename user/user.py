102fbb43d8abdc63f621e0a1493f04bc5311e21d
zineddineIMT <146106879+zineddineIMT@users.noreply.github.com>
2023-11-21 14:00:28 +0100

    add createbooking point

--- user/user.py
+++ user/user.py
@@ -59,6 +59,31 @@ def getUserBookingMovies(userId):
     except requests.RequestException as e:
         return jsonify({"error": str(e)}), 500
 
+
+@app.route("/users/<userId>/bookings/add", methods=['POST'])
+def addBookingForUser(userId):
+    # Récupérer les données de la requête entrante
+    booking_data = request.get_json()
+
+    # URL du service Booking
+    booking_service_url = "http://localhost:3201/bookings/{}".format(userId)
+
+    try:
+        # Envoyer les données de réservation au service Booking
+        booking_response = requests.post(booking_service_url, json=booking_data)
+
+        # Vérifier la réponse du service Booking
+        if booking_response.status_code == 200:
+            return jsonify(booking_response.json()), 200
+        else:
+            # Gérer les erreurs possibles du service Booking
+            return jsonify({"error": "Booking service responded with an error",
+                            "details": booking_response.json()}), booking_response.status_code
+    except requests.RequestException as e:
+        # Gérer les erreurs de connexion au service Booking
+        return jsonify({"error": "Failed to connect to Booking service", "details": str(e)}), 500
+
+
 if __name__ == "__main__":
    print("Server running in port %s"%(PORT))
    app.run(host=HOST, port=PORT)
