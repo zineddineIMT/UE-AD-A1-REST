10df67f73e7ef9d540c9e1b2f0c865f7b985d72b
zineddineIMT <146106879+zineddineIMT@users.noreply.github.com>
2023-11-24 19:33:46 +0100

    add verification for add booking method

--- booking/booking.py
+++ booking/booking.py
@@ -8,11 +8,13 @@ app = Flask(__name__)
 PORT = 3201
 HOST = '0.0.0.0'
 
+
 # Route pour la page d'accueil
 @app.route("/", methods=['GET'])
 def home():
     # Renvoie un message de bienvenue
     return "<h1 style='color:blue'>Welcome to the Booking service!</h1>"
+
 
 # Route pour obtenir toutes les réservations
 @app.route("/bookings", methods=['GET'])
@@ -20,12 +22,14 @@ def get_all_bookings_route():
     # Utilise la fonction get_all_bookings pour récupérer les réservations
     return jsonify({"bookings": bops.get_all_bookings()})
 
+
 # Route pour obtenir les réservations d'un utilisateur spécifique
 @app.route("/bookings/<userid>", methods=['GET'])
 def get_bookings_for_user_route(userid):
     # Récupère les réservations pour un utilisateur et renvoie une réponse adaptée
     user_bookings, status_code = bops.get_bookings_for_user(userid)
     return make_response(jsonify(user_bookings), status_code)
+
 
 # Route pour ajouter une réservation pour un utilisateur
 @app.route("/bookings/<userid>", methods=['POST'])
@@ -35,6 +39,7 @@ def add_booking_by_user_route(userid):
     # Ajoute la réservation et renvoie une réponse adaptée
     response, status_code = bops.add_booking_by_user(userid, booking_data)
     return make_response(jsonify(response), status_code)
+
 
 # Point d'entrée principal pour exécuter l'application Flask
 if __name__ == "__main__":
--- booking/booking_operations.py
+++ booking/booking_operations.py
@@ -2,23 +2,28 @@ import json
 import requests
 from datetime import datetime
 
+
 # Fonction pour charger les réservations depuis un fichier JSON
 def load_bookings():
     with open('./databases/bookings.json', "r") as jsf:
         return json.load(jsf)["bookings"]
 
+
 # Initialisation des réservations
 bookings = load_bookings()
+
 
 def get_all_bookings():
     # Renvoie toutes les réservations chargées
     return bookings
+
 
 def get_bookings_for_user(userid):
     # Filtrage des réservations par ID utilisateur
     user_bookings = [booking for booking in bookings if booking["userid"] == userid]
     # Renvoie les réservations trouvées ou un message d'erreur si aucune n'est trouvée
     return (user_bookings, 200) if user_bookings else ({"error": "No bookings found for this user"}, 404)
+
 
 def add_booking_by_user(userid, booking_data):
     # Vérifie si les champs nécessaires sont présents dans les données de réservation
@@ -39,11 +44,13 @@ def add_booking_by_user(userid, booking_data):
     except requests.exceptions.RequestException as e:
         return {"error": str(e)}, 500
 
-    # Vérifie si l'utilisateur a déjà une réservation pour cette date
+    # Vérifie si l'utilisateur a déjà une réservation pour cette date et ce film
     for booking in bookings:
         if booking['userid'] == userid:
-            if any(d['date'] == booking_data['date'] for d in booking['dates']):
-                return {"error": "An existing item already exists"}, 409
+            for d in booking['dates']:
+                if d['date'] == booking_data['date']:
+                    if booking_data['movieid'] in d['movies']:
+                        return {"error": "An existing item already exists"}, 409
 
     # Ajoute la réservation pour un utilisateur existant ou crée une nouvelle entrée pour un nouvel utilisateur
     new_booking = {"date": booking_data['date'], "movies": [booking_data['movieid']]}
